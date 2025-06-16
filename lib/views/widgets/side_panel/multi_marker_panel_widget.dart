e31ea23174e779e0cf9f4604d23834e11d724ab3
martonszucs <mszucs@hearsaycorp.com>
2025-06-16 22:24:49 +0200

    marker refined

--- lib/views/widgets/side_panel/multi_marker_panel_widget.dart
+++ lib/views/widgets/side_panel/multi_marker_panel_widget.dart
@@ -23,7 +23,7 @@ class MultiMarkerPanel extends StatelessWidget {
     
     return Container(
       width: isDesktop ? screenWidth * 0.33 : screenWidth - 40,
-      height: screenHeight * 0.85,
+      height: screenHeight * 0.90,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(16),
